5036d5a84268308ab01f8a748ac7d55be2695ab0
Mehbark <clarkemailtest@gmail.com>
2021-09-25 19:23:18 -0400

    sliiiiiiiiiiick pog

--- discord/bot.js
+++ discord/bot.js
@@ -1,1 +1,17 @@
-console.log("hello, world!");
+const Discord = require("discord.js");
+const lib = require("./botLibrary");
+const client = new Discord.Client({ intents: ["GUILD_MESSAGES"] });
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on("message", msg => {
+  if (msg.content === "ping") {
+    msg.reply("Pong!");
+  }
+});
+
+setTimeout(() => {
+  lib.connectBot(client);
+}, 3000);
--- discord/botLibrary.js
+++ discord/botLibrary.js
@@ -1,27 +1,44 @@
-const ws = require("ws");
+const WebSocket = require("ws").WebSocket;
 const fs = require("fs");
 
-function loadUsers(path = "./userClients.json", encoding = "utf8") {
+function readFile(path, encoding = "utf8") {
   try {
     return fs.readFileSync(path, encoding);
   } catch (err) {
     throw err;
   }
 }
+function loadUsers(path = "discord/userClients.json", encoding = "utf8") {
+  readFile(path, encoding);
+}
 
-function connectBot(url="ws://localhost:8080") {
-  const ws = new WebSocket("ws://localhost:8080");
+function connectBot(client, url = "ws://localhost:8080") {
+  client.login(readFile("privateData/.token"));
+
+  const ws = new WebSocket(url);
 
   ws.onopen = function () {
     ws.send(`{"intent":"spectate"}`);
+    console.log("Should be connected lol");
   };
 
   ws.onmessage = function (message) {
     const world = JSON.parse(message.data);
-    render(world, renderLocation);
+    console.log(message.data);
+    //render(world, renderLocation);
   };
 }
 
-function connectUser(username, password, path) {
-
+function render(world) {
+  return;
 }
+
+function connectUser(username, password, path) {
+}
+
+module.exports = {
+  readFile: readFile,
+  loadUsers: loadUsers,
+  connectBot: connectBot,
+  connectUser: connectUser
+};
